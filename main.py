4cc3952c7dde4672fe0bf51cfb515910b95c93a3
Rahmatjon <plungedincode@gmail.com>
2023-07-17 02:36:51 +0500

    nltk

---
+++ main.py
@@ -0,0 +1,6 @@
+from models import HiddenWordGenerator, WordlyGame
+
+if __name__ == "__main__":
+    wordly_game = WordlyGame()
+    wordly_game.play()
+
--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from colorama import Fore, Style
 from random_word import Wordnik
-
+import nltk
 
 class WordlyGame:
-    def __init__(self, hidden_word : str) -> None:
-        self.hidden_word = hidden_word
+    def __init__(self: str) -> None:
+        self.Validator = InputValidator()
+        self.Generator = HiddenWordGenerator()
         self.attempts = 6
         self.guessed_words = []
         self.win = False
-        # self.dict = enchant.Dict("en_US")
     
     def play(self):
         self.game_instruction()
@@ -17,14 +17,13 @@ class WordlyGame:
             guess = input("Enter your guess: ").strip().lower()
 
             if guess == "/show_attempts":
-                print("Guessed words : ", "\n".join(self.guessed_words))
-                continue
-
-            if not InputValidator.is_valid_guess(guess):
-                print("Invalid guess. Please enter a 5-letter word in english.")
+                print("Guessed words :\n", "\n".join(self.guessed_words))
                 continue
             
-            if guess == self.hidden_word:
+            if not self.check_guess(guess):
+                continue
+
+            if guess == self.Generator.hidden_word:
                 self.win = True
             
             checked_guess = self.check(guess)
@@ -35,16 +34,30 @@ class WordlyGame:
         
         if not self.win:
             print("Game over. You ran out of attempts.")
-            print(f"The hidden word was: {self.hidden_word}")
+            print(f"The hidden word was: {self.Generator.hidden_word}")
         else:
             print("Congratulations! You guessed the word.")
     
+
+    def check_guess(self, guess):
+        if not self.Validator.is_valid_guess(guess):
+            print("Invalid guess. Please enter a 5-letter word in english.")
+            return False
+        elif not self.Validator.is_english_word(guess):
+            print("There's no such word in English.")
+            return False
+        elif self.check(guess) in self.guessed_words:
+            print("Please try new word")
+            return False
+        return True
+    
+
     def check(self, guess):
         checked_guess = ""
         for i in range(5):
-            if guess[i] == self.hidden_word[i]:
+            if guess[i] == self.Generator.hidden_word[i]:
                 checked_guess += (Fore.GREEN + guess[i] + Style.RESET_ALL)
-            elif guess[i] in self.hidden_word:
+            elif guess[i] in self.Generator.hidden_word:
                 checked_guess += (Fore.YELLOW + guess[i] + Style.RESET_ALL)
             else:
                 checked_guess += (Fore.RED + guess[i] + Style.RESET_ALL)
@@ -69,6 +82,7 @@ class HiddenWordGenerator:
     # TODO : Build Wordnik with my own API key
     def __init__(self) -> None:
         self.Generator = Wordnik()
+        self.hidden_word = self.generate_hidden_word()
 
     def generate_hidden_word(self) -> str:
         word = self.Generator.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun", 
@@ -76,18 +90,18 @@ class HiddenWordGenerator:
         while word is None or '-' in word:
             word = self.Generator.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun", 
                                                   minDictionaryCount=3, minLength=5, maxLength=5)
-        return word
+        return word.lower()
 
 
 class InputValidator:
+    def __init__(self) -> None:
+        nltk.download('words') 
+
     @staticmethod
-    def is_valid_guess(guess:str) -> bool:
+    def is_english_word(word):
+        english_words = set(nltk.corpus.words.words())
+        return word.lower() in english_words
+
+    @staticmethod
+    def is_valid_guess(guess: str) -> bool:
         return len(guess) == 5 and guess.isalpha()
-
-
-if __name__ == "__main__":
-    hidden_word_generator = HiddenWordGenerator()
-    hidden_word = hidden_word_generator.generate_hidden_word()
-    print(hidden_word)
-    wordly_game = WordlyGame(hidden_word)
-    wordly_game.play()
